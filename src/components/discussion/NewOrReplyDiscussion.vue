b12a39f0b56573911a07735618955fc35d83b6e5
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-01-25 12:53:58 +0330

    new or reply discussion ui

---
+++ src/components/discussion/NewOrReplyDiscussion.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex">
+    <rounded-image :title="'M e'"/>
+    <v-text-field
+      rounded-xl
+      :label="isReply ? 'Reply' : 'Start a discussion'"
+      flat
+      class="ml-3 my-auto"
+      solo
+      outlined
+      dense
+      hide-details=""
+    ></v-text-field>
+  </div>
+</template>
+
+<script>
+import RoundedImage from "../widget/RoundedImage.vue";
+export default {
+  components: { RoundedImage },
+  props: {
+    isReply: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
