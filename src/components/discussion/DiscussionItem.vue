b25acb615683c8e8d778cd5d6fca2721c455c17f
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-01-25 12:55:32 +0330

    discussion item ui

---
+++ src/components/discussion/DiscussionItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-layout>
+      <rounded-image
+        :src="discussion.user.avatar"
+        :title="discussion.user.name"
+      />
+      <div class="ml-2 mt-1">
+        <div class="d-flex">
+          <h4>{{ discussion.user.name }}</h4>
+          <span class="mx-3 grey--text">{{
+            getTimePassed(discussion.date)
+          }}</span>
+        </div>
+
+        <p>{{ discussion.text }}</p>
+        <v-layout> </v-layout>
+        <div class="d-flex">
+          <like :count="discussion.likes" :iLikedIt="discussion.iLikedIt" />
+          <strong v-if="isShowReply" @click="discussion.showReplies = true" class="mx-3 my-auto reply">Reply</strong>
+        </div>
+      </div>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import Like from "../widget/Like.vue";
+import RoundedImage from "../widget/RoundedImage.vue";
+export default {
+  components: { RoundedImage, Like },
+  props: {
+    discussion: {
+      type: Object,
+      default: null,
+    },
+    isShowReply: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.reply {
+  color: #0c589d;
+}
+</style>
---
+++ src/components/widget/Like.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-chip class="px-4" :color="iLikedIt ? '#245ee7' : '#F4F5FA'">
+      <v-avatar left>
+        <v-icon small :color="iLikedIt ? '#fff' : '#909AB4'">mdi-thumb-up</v-icon>
+      </v-avatar>
+      <strong :class="iLikedIt ? 'white--text' : ''">{{ count }}</strong>
+    </v-chip>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+    iLikedIt: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
